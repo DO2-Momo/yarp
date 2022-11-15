449b7423171a439f5c60b6f5d6b39004d41adb45
matiasvlevi <matiasvlevi@gmail.com>
2022-11-15 05:09:04 -0500

    latest

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -8,6 +8,9 @@ use gtk::{
   Justification,
   Align,
   EntryBuffer,
+  Entry,
+  Scale,
+  PositionType,
   Button
 };
 use gtk::prelude::*;
@@ -26,6 +29,7 @@ pub fn package_profile_menu(
 
   let package_profile_box = Box::builder()
   .orientation(Orientation::Vertical)
+  .valign(Align::Start)
   .css_classes(vec![String::from("profile")])
   .build();
 
@@ -35,6 +39,120 @@ pub fn package_profile_menu(
   package_profile_box.append(&chk_nightly);
 
   return package_profile_box;
+}
+
+pub fn get_package_profile_box(
+  chk_desktop: &CheckButton,
+  chk_utils: &CheckButton,
+  chk_multimedia: &CheckButton,
+  chk_nightly: &CheckButton
+) -> Box {
+  let package_label = Label::builder()
+    .label("Packages:")
+    .justify(Justification::Left)
+    .xalign(0.0)
+    .css_classes(vec![String::from("section-title")])
+    .build();
+
+  let package_profile_box = Box::builder()
+    .orientation(Orientation::Vertical)
+    .halign(Align::Start)
+    .valign(Align::Start)
+    .css_classes(vec![String::from("package-box")])
+    .build();
+
+  package_profile_box.append(&package_label);
+  package_profile_box.append(chk_desktop);
+  package_profile_box.append(chk_utils);
+  package_profile_box.append(chk_multimedia);
+  package_profile_box.append(chk_nightly);
+
+  return package_profile_box;
+} 
+
+
+pub fn get_partition_box(
+  swap_scale: &Scale,
+  part_scale: &Scale,
+  root_home_ratio_label: &Label
+) -> Box {
+
+  let partition_label = Label::builder()
+    .label("Partitions:")
+    .justify(Justification::Left)
+    .xalign(0.0)
+    .css_classes(vec![String::from("section-title")])
+    .build();
+
+  let swap_label = Label::builder()
+    .label("Swap")
+    .justify(Justification::Left)
+    .xalign(0.0)
+    .css_classes(vec![String::from("section-title-2")])
+    .build();
+
+  swap_scale.set_value(4096.0);
+  swap_scale.set_increments(256.0, 1024.0);
+  swap_scale.set_range(512.0, 8129.0);
+  swap_scale.add_mark(2048.0, PositionType::Bottom,  Some("2048 Mb"));
+  swap_scale.add_mark(4096.0, PositionType::Bottom,  Some("4096 Mb"));
+  swap_scale.add_mark(8192.0, PositionType::Bottom,  Some("8192 Mb"));
+  swap_scale.set_draw_value(true);
+
+  part_scale.set_value(50.0);
+  part_scale.set_increments(1.0, 5.0);
+  part_scale.set_range(25.0, 100.0);
+  part_scale.add_mark(25.0, PositionType::Bottom,  Some("25%"));
+  part_scale.add_mark(50.0, PositionType::Bottom,  Some("50%"));
+
+  part_scale.add_mark(75.0, PositionType::Bottom,  Some("75%"));
+  part_scale.add_mark(100.0, PositionType::Bottom, Some("100%"));
+  part_scale.set_draw_value(true);
+
+// get 
+
+  let size_label = Label::builder().label("Root: Home:").build();
+
+  // let slider_wrap = Box::builder()
+  //   .css_classes(vec![String::from("slider-wrap")])
+  //   .hexpand(true).vexpand(true)
+  //   .build();
+    
+  // slider_wrap.append(&scale_part_ratio);
+
+  let partition_box = Box::builder()
+    .orientation(Orientation::Vertical)
+    .hexpand(true)
+    .vexpand(true)
+    .css_classes(vec![String::from("partition-box")])
+    .build();
+
+  let partition_slider_box = Box::builder()
+    .orientation(Orientation::Horizontal)
+    .hexpand(true)
+    .vexpand(true)
+    .css_classes(vec![String::from("partition-box")])
+    .build();
+
+
+  let swap_size_input = Entry::builder()
+    .input_purpose(InputPurpose::Digits)
+    .css_classes(vec![String::from("swap-field")])
+    .build();
+
+
+  partition_box.append(&partition_label);
+
+
+  partition_box.append(&swap_label);
+  partition_box.append(swap_scale);
+
+  partition_box.append(root_home_ratio_label);
+  partition_box.append(part_scale);
+  partition_box.append(&size_label);
+
+
+  return partition_box;
 }
 
 pub fn formField(label: &str, buf: &EntryBuffer, hidden: bool ) -> Box {
@@ -72,7 +190,14 @@ pub fn formField(label: &str, buf: &EntryBuffer, hidden: bool ) -> Box {
   return field;
 }
 
-pub fn form() -> Form {
+pub fn form(name: &str) -> Form {
+
+  let title = Label::builder()
+    .label(name)
+    .justify(Justification::Left)
+    .xalign(0.06)
+    .css_classes(vec![String::from("section-title")])
+    .build();
 
   let ubuf = EntryBuffer::builder().build();
   let hbuf = EntryBuffer::builder().build();
@@ -97,6 +222,12 @@ pub fn form() -> Form {
   let password = formField("password ", &formData.password,true);
   let cpassword = formField("confirm password ", &formData.cpassword, true);
 
+  let container: Box = Box::builder()
+      .orientation(Orientation::Vertical)
+      .hexpand(false)
+      .vexpand(false)
+      .css_classes(vec![String::from("form-section")])
+      .build();
 
   let form: Box = Box::builder()
       .orientation(Orientation::Vertical)
@@ -110,9 +241,12 @@ pub fn form() -> Form {
   form.append(&username);
   form.append(&password);
   form.append(&cpassword);
+
+  container.append(&title);
+  container.append(&form);
   
   return Form {
-    widget: form,
+    widget: container,
     data: formData
   };
 }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ mod install;
 mod config;
 
 use crate::config::{User, UserData};
-use crate::sysinfo::Devices;
+use crate::sysinfo::{Devices, Partition};
 use crate::components::prompt_user;
 use crate::components::control::{PackageProfile};
+
+use crate::install::slashdev;
 use std::{str};
 use gtk::gdk::Display;
 use gtk::prelude::*;
-use gtk::gio::SimpleAction;
+use gtk::gio::{ListModel, SimpleAction};
 use gtk::glib;
 use glib_macros::clone;
 use gtk::{
@@ -94,8 +96,11 @@ fn get_device_sizes(devices: &sysinfo::Devices) -> Vec<u128> {
 
 fn build_ui(app: &Application) {
   // Get devices data
-  let device_data: sysinfo::Devices = sysinfo::get_devices();
+  let mut device_data: sysinfo::Devices = sysinfo::get_devices();
   let device_data_sizes: Vec<u128> = get_device_sizes(&device_data);
+
+  static mut device_select: usize = 0;
+
   // Create a button with label and margins
   let confirm_button = Button::builder()
     .label("Confirm")
@@ -108,7 +113,6 @@ fn build_ui(app: &Application) {
     .css_classes(vec![String::from("main"),String::from("box")])
     .build();
 
-  let form = components::form();
 
   let flex = Box::builder()
     .orientation(Orientation::Horizontal)
@@ -117,48 +121,6 @@ fn build_ui(app: &Application) {
     .css_classes(vec![String::from("flex-div")])
     .build();
   
-  let right_box = Box::builder()
-    .orientation(Orientation::Vertical)
-    .hexpand(true)
-    .vexpand(true)
-    .css_classes(vec![String::from("right-box")])
-    .width_request(400)
-    .build();
-
-  let partition_label = Label::builder()
-    .label("Partitions:")
-    .justify(Justification::Left)
-    .xalign(0.0)
-    .css_classes(vec![String::from("section-title")])
-    .build();
-  
-  let swap_label = Label::builder()
-    .label("Swap")
-    .justify(Justification::Left)
-    .xalign(0.0)
-    .css_classes(vec![String::from("section-title-2")])
-    .build();
-      
-  let root_home_ratio_label = Label::builder()
-    .label("home/root ratio")
-    .justify(Justification::Left)
-    .xalign(0.0)
-    .css_classes(vec![String::from("section-title-2")])
-    .build();
-
-  let package_label = Label::builder()
-    .label("Packages:")
-    .justify(Justification::Left)
-    .xalign(0.0)
-    .css_classes(vec![String::from("section-title")])
-    .build();
-
-  let package_profile_box = Box::builder()
-    .orientation(Orientation::Vertical)
-    .halign(Align::Start)
-    .valign(Align::Start)
-    .css_classes(vec![String::from("package-box")])
-    .build();
 
   // Create a window
   let window = ApplicationWindow::builder()
@@ -172,37 +134,21 @@ fn build_ui(app: &Application) {
   let device_labels_dyn: Vec<String> = get_device_labels(&device_data);
   let device_labels: Vec<&str> = device_labels_dyn.iter().map(|s| s as &str).collect();
 
+  // Package manager bundles
+
   let chk_desktop:CheckButton    = CheckButton::with_label("Desktop Environement");
   let chk_utils:CheckButton      = CheckButton::with_label("Desktop Features");
   let chk_multimedia:CheckButton = CheckButton::with_label("Multimedia Utils");
   let chk_nightly:CheckButton    = CheckButton::with_label("Nightly pack");
 
-  package_profile_box.append(&package_label);
-  package_profile_box.append(&chk_desktop);
-  package_profile_box.append(&chk_utils);
-  package_profile_box.append(&chk_multimedia);
-  package_profile_box.append(&chk_nightly);
+  let package_profile_box = components::get_package_profile_box(
+    &chk_desktop, &chk_utils, &chk_multimedia, &chk_nightly
+  );
 
-  let scale_swap = Scale::builder()
-    .orientation(Orientation::Horizontal)
-    .width_request(275)
-    .height_request(80)
-    .value_pos(PositionType::Top)
-    .has_origin(true)
-    .can_target(true)
-    .fill_level(16384.0)
-    .vexpand(false)
-    .build();
 
-  scale_swap.set_value(4096.0);
-  scale_swap.set_increments(256.0, 1024.0);
-  scale_swap.set_range(512.0, 8129.0);
-  scale_swap.add_mark(2048.0, PositionType::Bottom,  Some("2048 Mb"));
-  scale_swap.add_mark(4096.0, PositionType::Bottom,  Some("4096 Mb"));
-  scale_swap.add_mark(8192.0, PositionType::Bottom,  Some("8192 Mb"));
-  scale_swap.set_draw_value(true);
+  // Partitions
 
-  let scale_part_ratio = Scale::builder()
+  let part_scale = Scale::builder()
     .orientation(Orientation::Horizontal)
     .width_request(275)
     .height_request(80)
@@ -213,60 +159,102 @@ fn build_ui(app: &Application) {
     .vexpand(false)
     .build();
 
-  scale_part_ratio.set_value(50.0);
-  scale_part_ratio.set_increments(1.0, 5.0);
-  scale_part_ratio.set_range(25.0, 100.0);
-  scale_part_ratio.add_mark(25.0, PositionType::Bottom,  Some("25%"));
-  scale_part_ratio.add_mark(50.0, PositionType::Bottom,  Some("50%"));
-
-  scale_part_ratio.add_mark(75.0, PositionType::Bottom,  Some("75%"));
-  scale_part_ratio.add_mark(100.0, PositionType::Bottom, Some("100%"));
-  scale_part_ratio.set_draw_value(true);
-
-  let size_label = Label::builder().label("Root: Home:").build();
-
-  // let slider_wrap = Box::builder()
-  //   .css_classes(vec![String::from("slider-wrap")])
-  //   .hexpand(true).vexpand(true)
-  //   .build();
-    
-  // slider_wrap.append(&scale_part_ratio);
-
-  let partition_box = Box::builder()
-    .orientation(Orientation::Vertical)
-    .hexpand(true)
-    .vexpand(true)
-    .css_classes(vec![String::from("partition-box")])
-    .build();
-
-  let partition_slider_box = Box::builder()
+  let swap_scale = Scale::builder()
     .orientation(Orientation::Horizontal)
+    .width_request(275)
+    .height_request(80)
+    .value_pos(PositionType::Top)
+    .has_origin(true)
+    .can_target(true)
+    .fill_level(16384.0)
+    .vexpand(false)
+    .build();
+
+  let root_home_ratio_label = Label::builder()
+    .label("Root / Home")
+    .justify(Justification::Left)
+    .xalign(0.0)
+    .css_classes(vec![String::from("section-title-2")])
+    .build();
+
+  let partition_box = components::get_partition_box(
+    &swap_scale,
+    &part_scale,
+    &root_home_ratio_label
+  );
+  
+  // Right Container
+  let right_box = Box::builder()
+    .orientation(Orientation::Vertical)
+    .valign(Align::Start)
     .hexpand(true)
     .vexpand(true)
-    .css_classes(vec![String::from("partition-box")])
+    .css_classes(vec![String::from("right-box")])
+    .width_request(400)
     .build();
-
-
-  let swap_size_input = Entry::builder()
-    .input_purpose(InputPurpose::Digits)
-    .css_classes(vec![String::from("swap-field")])
-    .build();
-
-
-  partition_box.append(&partition_label);
-
-
-  partition_box.append(&swap_label);
-  partition_box.append(&scale_swap);
-
-  partition_box.append(&root_home_ratio_label);
-  partition_box.append(&scale_part_ratio);
-  partition_box.append(&size_label);
 
   right_box.append(&partition_box);
   right_box.append(&package_profile_box);
 
+  let form = components::form("Host & Login:");
 
+  // let device_selector = Box::builder()
+  //   .orientation(Orientation::Vertical)
+  //   .css_classes(vec![String::from("device-selector")])
+  //   .build();
+
+  // let mut device_widgets: Vec<Box> = Vec::<Box>::new();
+
+  // for i in 0..device_labels.len() {
+
+  //   let device_element = Box::builder()
+  //   .css_classes(vec![String::from("device-element")])
+  //     .homogeneous(true)
+  //   .halign(Align::Baseline)
+  //   .build();
+
+  //   device_element.append(
+  //     &Label::builder()
+  //       .label(&install::slashdev(&device_data.blockdevices[i].name, 0))
+  //       .css_classes(vec![String::from("device-element-p")])
+  //       .build()
+  //   );
+
+  //   device_element.append(
+  //     &Label::builder()
+  //       .label(&format!("{} GB", (device_data.blockdevices[i].size) / 1000000000))
+  //       .css_classes(vec![String::from("device-element-p")])
+  //       .build()
+  //   );
+
+  //   let partitions: i32 = device_data.blockdevices[i].children.as_mut().unwrap().len().try_into().unwrap();
+
+  //   device_element.append(&Label::builder()
+  //     .label(&format!("{}", partitions))
+  //     .css_classes(vec![String::from("device-element-p")])
+  //     .build()
+  //   );
+
+  //   let device_element_button = Button::builder()
+  //   .css_classes(vec![String::from("device-button")])
+  //   .child(&device_element)
+  //   .build();
+
+  //   device_element_button.set_property::<&str>("name", &format!("{}", i));
+
+  //   device_element_button.connect_clicked(|device_element_button| {
+  //     println!("Hello world {}", device_element_button.property_value("name").as_ptr())
+  //   });
+
+  //   device_selector.append(&device_element_button);
+  // }
+
+  let device_menu_label = Label::builder()
+    .label("Device:")
+    .justify(Justification::Left)
+    .xalign(0.0)
+    .css_classes(vec![String::from("section-title")])
+    .build();
 
   // Create dropdown menu with the labels
   let device_menu = DropDown::from_strings(&device_labels);
@@ -285,6 +273,7 @@ fn build_ui(app: &Application) {
     .css_classes(vec![String::from("dropdown")])
     .build();
 
+  device_box.append(&device_menu_label);
   device_box.append(&device_menu);
 
   let original_state:i32 = 0;
@@ -293,39 +282,6 @@ fn build_ui(app: &Application) {
     Some(&i32::static_variant_type()),
     &original_state.to_variant(),
   );
-
-  // scale_part_ratio.connect_value_changed( move |scale_part_ratio| {
-  //   // Activate "win.count" and pass "1" as parameter
-  //   let device: &sysinfo::Device =
-  //     device_data.get(device_menu.selected() as usize);
-
-  //   let space_size:i32 = ((device.size/1000000) as i32)  - scale_swap.value() as i32 - 300;
-
-  //   let param = scale_part_ratio.value() as i32 * space_size;
-  //   scale_part_ratio
-  //     .activate_action("win.set_root_size", Some(&param.to_variant()))
-  //     .expect("The action does not exist.");
-  // });
-
-
-
-  // set_root_size.connect_activate(clone!(@weak root_size_label => move |action, param| {
-  //   let mut state = action
-  //     .state()
-  //     .expect("Could not get state.")
-  //     .get::<i32>()
-  //     .expect("The variant needs to be of type `i32`.");
-
-  //   // Get parameter
-  //   let param = param
-  //     .expect("Could not get parameter.")
-  //     .get::<i32>()
-  //     .expect("The variant needs to be of type `i32`.");
-
-  //   state = param;
-
-  //   root_size_label.set_label(&format!("{state} Mb"));
-  // }));
 
   // Connect to "clicked" signal of `confirm_button`
   confirm_button.connect_clicked(move |confirm_button| {
@@ -350,27 +306,33 @@ fn build_ui(app: &Application) {
       nightly:    chk_nightly.is_active()
     };
 
-    let space_size:u64 = ((device.size/1000000) as u64)  - scale_swap.value() as u64 - 300;
+    let space_size:u64 = ((device.size/1000000) as u64)  - swap_scale.value() as u64 - 300;
     println!("space_size {}", space_size);
     let userData = UserData {
       user, hostname, 
       device: device,
       packages: packages,
-      swap: scale_swap.value() as u64,
-      ratio: scale_part_ratio.value(),
-      root: ((scale_part_ratio.value()/100.0) * space_size as f64) as u64,
-      home: (((100.0 - scale_part_ratio.value())/100.0) * space_size as f64) as u64 
+      swap: swap_scale.value() as u64,
+      ratio: part_scale.value(),
+      root: ((part_scale.value()/100.0) * space_size as f64) as u64,
+      home: (((100.0 - part_scale.value())/100.0) * space_size as f64) as u64 
     };
 
     // Validate configuration & Start installation if is valid
     validation::validate_config(&userData);
   });
 
+  // part_scale.connect_value_pos_notify(|part_scale| {
+  //   root_home_ratio_label.set_label(&format!("Root {} / Home {} ", part_scale.value(), 100-part_scale.value()))
+  // });
+
   // Add to main continer 
+
   main_box.append(&device_box);
   flex.append(&form.widget);
   flex.append(&right_box);
   main_box.append(&flex);
+
   main_box.append(&confirm_button);
   window.add_action(&set_root_size);
 
