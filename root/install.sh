b5adb02fc15e4ea19bf2932795dedab2a8afad51
vlev <matiasvlevi@gmail.com>
2023-03-26 19:26:51 +0000

    grub legacy or uefi install script
    Argument #3 determines whether or not the script should install a legacy
    or uefi bootloader with grub.
    
    "true" is for legacy
    "false" is for uefi
    
    The device's name is passed in Argument #4

--- root/install.sh
+++ root/install.sh
@@ -1,4 +1,23 @@
 #
+# Install & configure bootloader
+#
+
+if [ "$3" == "false" ];
+then
+  grub-install \
+    --target=x86_64-efi \
+    --efi-directory=/boot/efi \
+    --bootloader-id=DO2OS \
+    --recheck \
+    --removable
+else 
+  grub-install --target=i386-pc $4
+fi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+#
 # Users & Passwords
 #
 
@@ -9,17 +28,6 @@ echo -e "$2\\n$2" | passwd
 useradd -m -G wheel -s /bin/bash $1
 echo -e "$2\\n$2" | passwd $1 
 
-#
-# Install & configure bootloader
-#
-grub-install \
-  --target=x86_64-efi \
-  --efi-directory=/boot/efi \
-  --bootloader-id=DO2OS \
-  --removable \
-  --recheck
-
-grub-mkconfig -o /boot/grub/grub.cfg
 
 # Generate Locale
 locale-gen
@@ -30,6 +38,7 @@ mkdir /home/$1/Downloads;
 mkdir /home/$1/Documents;
 mkdir /home/$1/Pictures;
 mkdir /home/$1/Videos;
+mkdir /home/$1/Music;
 
 chown -R $1 /home/$1
 
@@ -41,6 +50,7 @@ systemctl enable sddm.service
 
 # Install AUR 
 echo "$2" | su - $1 -c "source /home/$1/.install_aur.sh $2"
+
 
 # Remove self
 rm -f /install.sh
